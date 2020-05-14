92f0a108b3d0a7debb77c6ce4a96a7d54fea1b39
Gabriel Sandu <allmediacreation@gmail.com>
2020-05-14 09:51:12 +0300

    Updates

---
+++ precache-manifest.82ddeef499d68b6fcc8874edec0a8512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5fe38fc7ad269c8c4a2c9fa8e7fb37",
+    "url": "/Flickr-photo/index.html"
+  },
+  {
+    "revision": "09fe8c1dd6c3aeb23188",
+    "url": "/Flickr-photo/static/css/main.73a26ec7.chunk.css"
+  },
+  {
+    "revision": "cca12eaeea840926e657",
+    "url": "/Flickr-photo/static/js/2.b36a6d18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Flickr-photo/static/js/2.b36a6d18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09fe8c1dd6c3aeb23188",
+    "url": "/Flickr-photo/static/js/main.f7df8555.chunk.js"
+  },
+  {
+    "revision": "5b9b7d9b69910ac6b7f0",
+    "url": "/Flickr-photo/static/js/runtime-main.c8bfc9c6.js"
+  }
+]);
